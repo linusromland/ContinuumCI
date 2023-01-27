75c7357b4f7539f7e32e371ac21e9963c233d51c
Linus Romland <hello@linusromland.com>
2023-01-27 19:30:54 +0100

    Implemented mongodb connection

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,14 @@
 import { Module } from '@nestjs/common';
 
 // Internal dependencies
+import { databaseProviders, schemaProviders } from './providers';
+
+// Modules import
 import { WelcomeModule } from './routes/welcome/welcome.module';
 
 @Module({
-	imports: [WelcomeModule]
+	imports: [WelcomeModule],
+	providers: [...databaseProviders, ...schemaProviders],
+	exports: [...databaseProviders, ...schemaProviders]
 })
 export class AppModule {}
