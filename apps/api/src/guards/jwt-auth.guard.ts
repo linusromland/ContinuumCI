744e1904ce392382d5e859f14f7c0e69ded522fb
Linus Romland <hello@linusromland.com>
2023-01-29 11:19:00 +0100

    Update jwt-auth.guard.ts

--- apps/api/src/guards/jwt-auth.guard.ts
+++ apps/api/src/guards/jwt-auth.guard.ts
@@ -1,6 +1,26 @@
 // External dependencies
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Inject, UnauthorizedException } from '@nestjs/common';
+import { Model } from 'mongoose';
 import { AuthGuard } from '@nestjs/passport';
 
+// Internal dependencies
+import { UserType } from 'shared/src/types';
+
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+	//inject user model
+	constructor(@Inject('USER_MODEL') private UserModel: Model<UserType>) {
+		super();
+	}
+
+	canActivate(context: ExecutionContext) {
+		return super.canActivate(context);
+	}
+
+	handleRequest(err, user) {
+		if (err || !user) {
+			throw err || new UnauthorizedException();
+		}
+		return user;
+	}
+}
