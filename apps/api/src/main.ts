cfcef3dad1470f2809bec9f3e5b5caae71b88367
Linus Romland <hello@linusromland.com>
2023-02-03 17:11:37 +0100

    Removed API Docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,11 @@
 // External dependencies
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 // Internal dependencies
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-
-	const config = new DocumentBuilder()
-		.setTitle('ContinuumCI API')
-		.setDescription('The API Documentation for ContinuumCI')
-		.setVersion('1.0')
-		.build();
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api/docs', app, document);
-
 	await app.listen(3000);
 }
 bootstrap();
--- apps/api/src/routes/welcome/welcome.controller.ts
+++ apps/api/src/routes/welcome/welcome.controller.ts
@@ -1,24 +1,15 @@
 // External dependencies
 import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 // Internal dependencies
 import { WelcomeService } from './welcome.service';
 import { WelcomeType } from 'shared/src/types';
-import { WelcomeEntity } from './entities/welcome.entity';
 
-@ApiTags('Welcome')
 @Controller()
 export class WelcomeController {
 	constructor(private readonly welcomeService: WelcomeService) {}
 
 	@Get()
-	@ApiOperation({ summary: 'Get the welcome message' })
-	@ApiResponse({
-		status: 200,
-		description: 'The welcome message',
-		type: WelcomeEntity
-	})
 	getWelcomeMessage(): WelcomeType {
 		return this.welcomeService.getWelcomeMessage();
 	}
