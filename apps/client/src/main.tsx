6d39524ebe06224418942d6cca3685c1397dba6a
Linus Romland <hello@linusromland.com>
2023-02-05 10:15:46 +0100

    Added React Router

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// External dependencies
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Internal dependencies
+import Router from './routes';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>
+);
