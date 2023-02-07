7df584f21f8dc001948cf91348981f131b116b5d
Linus Romland <hello@linusromland.com>
2023-02-07 18:34:18 +0100

    Added explanation for the nginx exporter

--- apps/nginxExporter/src/app.ts
+++ apps/nginxExporter/src/app.ts
@@ -1,3 +1,10 @@
+/**
+ * This is used to export the nginx logs to MongoDB
+ * It is necessary that the logs are formatted in a specific way.
+ * The logs must be formatted like this:
+ * "remote_address__:__$remote_addr __|__remote_user__:__$remote_user __|__time_local__:__$time_local __|__request_method__:__$request_method __|__request_url__:__$request_uri __|__request_protocol__:__$server_protocol __|__status__:__$status __|__body_bytes_sent__:__$body_bytes_sent __|__http_referer__:__$http_referer __|__http_user_agent__:__$http_user_agent __|__http_x_forwarded_for__:__$http_x_forwarded_for"
+ * 
+ * This can be done by adding the following to the nginx config file:
 
 log_format custom_format 'remote_address__:__$remote_addr __|__remote_user__:__$remote_user __|__time_local__:__$time_local __|__request_method__:__$request_method __|__request_url__:__$request_uri __|__request_protocol__:__$server_protocol __|__status__:__$status __|__body_bytes_sent__:__$body_bytes_sent __|__http_referer__:__$http_referer __|__http_user_agent__:__$http_user_agent __|__http_x_forwarded_for__:__$http_x_forwarded_for';
 access_log /var/log/nginx/custom.log custom_format;
