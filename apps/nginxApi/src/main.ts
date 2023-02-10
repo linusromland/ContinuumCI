84bb358d0be27a63dba9252f6230735bbc4d0f26
Linus Romland <hello@linusromland.com>
2023-02-10 17:01:25 +0100

    Added warn for the nginx api

--- apps/nginxApi/src/main.ts
+++ apps/nginxApi/src/main.ts
@@ -1,5 +1,6 @@
 // External dependencies
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 
 // Internal dependencies
 import { AppModule } from './app.module';
@@ -7,5 +8,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	await app.listen(3001);
+	Logger.warn("Please note that this API is not meant to be used publicly. It is ONLY meant to be use within the same docker network to communicate with ContinuumCI's API", 'Nginx API');
 }
 bootstrap();
