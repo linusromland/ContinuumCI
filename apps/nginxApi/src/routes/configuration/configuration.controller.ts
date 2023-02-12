02e13721f0ff49146c69a860ac8ff1ec17ad9d02
Linus Romland <hello@linusromland.com>
2023-02-12 09:21:25 +0100

    Created validator for inputs

--- apps/nginxApi/src/routes/configuration/configuration.controller.ts
+++ apps/nginxApi/src/routes/configuration/configuration.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get, Put } from '@nestjs/common';
+// External dependencies
+import { BadRequestException, Body, Controller, Get, Put } from '@nestjs/common';
+import fs from 'fs';
 
 // Internal dependencies
 import {
@@ -7,6 +9,7 @@ import {
 	ResponseType
 } from 'shared/src/types';
 import { ConfigurationService } from './configuration.service';
+import argumentValidator from 'src/utils/argumentValidator';
 
 @Controller('configuration')
 export class ConfigurationController {
@@ -16,6 +19,59 @@ export class ConfigurationController {
 	async edit(
 		@Body() nginxConfiguration: NginxConfigurationType
 	): Promise<ResponseType> {
+		try{
+			argumentValidator(
+			[
+				{
+					argument: 'localIps',
+					type: 'string',
+					required: true
+				},
+				{
+					argument: 'sitesEnabledLocation',
+					type: 'string',
+					required: true
+				},
+				{
+					argument: 'domains',
+					type: 'object',
+					required: true,
+					array: true
+				}
+			],
+			nginxConfiguration
+		)
+	
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			throw new BadRequestException({
+				success: false,
+				message: error.message
+			});
+		}
+
+		const sitesEnabledLocation = nginxConfiguration.sitesEnabledLocation;
+		// Check if the sites-enabled location exists, is a directory and is writable
+		if (
+			!fs.existsSync(sitesEnabledLocation) ||
+			!fs.lstatSync(sitesEnabledLocation).isDirectory()
+		) {
+			throw new BadRequestException({
+				success: false,
+				message:
+					'The sites-enabled location does not exist or is not a directory'
+			});
+		}
+
+		try {
+			fs.accessSync(sitesEnabledLocation, fs.constants.W_OK);
+		} catch (error) {
+			throw new BadRequestException({
+				success: false,
+				message: 'The sites-enabled location is not writable'
+			});
+		}
+
 		return this.configurationService.edit(nginxConfiguration);
 	}
 
