5374497ab4265b04eb67ff9a53c7e77bdb5590e1
Linus Romland <hello@linusromland.com>
2023-02-12 16:01:02 +0100

    Added NGINX_API_URL to env in api

--- apps/api/src/utils/env.ts
+++ apps/api/src/utils/env.ts
@@ -8,3 +8,4 @@ export const MONGODB_URI =
 	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ContinuumCI';
 export const JWT_SECRET = process.env.JWT_SECRET || 'keyboard cat';
 export const API_HOST = process.env.API_HOST || 'http://localhost:3000';
+export const NGINX_API_URL = process.env.NGINX_API_URL || 'http://localhost:3001';
