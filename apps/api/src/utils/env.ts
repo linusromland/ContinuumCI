54ccd3841a2dc4f608da85100bf5532001cb4cf5
Linus Romland <hello@linusromland.com>
2023-02-12 15:54:30 +0100

    Added mongodb uri to api

--- apps/api/src/utils/env.ts
+++ apps/api/src/utils/env.ts
@@ -1,7 +1,10 @@
-/* eslint-disable turbo/no-undeclared-env-vars */
-// Conifgure dotenv
+// External dependencies
 import { config } from 'dotenv';
+
+// Configure dotenv
 config();
 
+export const MONGODB_URI =
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ContinuumCI';
 export const JWT_SECRET = process.env.JWT_SECRET || 'keyboard cat';
 export const API_HOST = process.env.API_HOST || 'http://localhost:3000';
