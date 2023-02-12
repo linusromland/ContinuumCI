4b48647c492ae3bb3bbf8021a7959cd8c12e86c9
Linus Romland <hello@linusromland.com>
2023-02-12 14:11:02 +0100

    Disabled turbo envs

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -13,6 +13,7 @@ module.exports = {
 	},
 	rules: {
 		'@typescript-eslint/no-explicit-any': 'error',
-		'@typescript-eslint/no-unused-vars': 'error'
+		'@typescript-eslint/no-unused-vars': 'error',
+		"turbo/no-undeclared-env-vars": "off",
 	}
 };
