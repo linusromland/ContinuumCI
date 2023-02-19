4c7ed66c2653c6ea8eb39bfc51d9c5d199e0529d
Linus Romland <hello@linusromland.com>
2023-02-19 16:19:57 +0100

    Fixed toast css issue

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 // Internal dependencies
 import './main.scss';
 import Router from './routes';
+
+// Toast CSS
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
