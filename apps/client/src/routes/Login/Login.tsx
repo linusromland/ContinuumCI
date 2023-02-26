7e50109cbea8c080dc068698673f39e7e53ae15c
Linus Romland <hello@linusromland.com>
2023-02-26 10:30:55 +0100

    Upadted login page

--- apps/client/src/routes/Login/Login.tsx
+++ apps/client/src/routes/Login/Login.tsx
@@ -1,10 +1,41 @@
+// External Dependencies
+import { useState } from 'react';
+
 // Internal Dependencies
+import style from './Login.module.scss';
 import SetupLayout from '../../components/SetupLayout/SetupLayout';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 export default function Login(): JSX.Element {
+	const [registered, setRegistered] = useState(false);
+
 	return (
 		<SetupLayout>
-			<>Login</>
+			<div className={style.container}>
+				<p className={style.subtitle}>
+					{registered
+						? 'Welcome! Please register for an account.'
+						: 'Welcome back! Please login to your account.'}
+				</p>
+				{registered ? (
+					<RegistrationForm onSubmit={console.log} />
+				) : (
+					<LoginForm onSubmit={console.log} />
+				)}
+
+				<p className={style.footerText}>
+					{registered
+						? 'Already have an account?'
+						: "Don't have an account?"}{' '}
+					<a
+						onClick={() => setRegistered(!registered)}
+						className={style.link}
+					>
+						{registered ? 'Login' : 'Register'}
+					</a>
+				</p>
+			</div>
 		</SetupLayout>
 	);
 }
