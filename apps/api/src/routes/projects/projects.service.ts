ef5614228141122b3557f60ca99220208e830dd1
Linus Romland <hello@linusromland.com>
2023-03-04 10:34:44 +0100

    Clones Git Repo on project create

--- apps/api/src/routes/projects/projects.service.ts
+++ apps/api/src/routes/projects/projects.service.ts
@@ -1,9 +1,11 @@
 // External dependencies
 import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
+import simpleGit from 'simple-git';
 
 // Internal dependencies
 import { ProjectClass } from 'shared/src/classes';
+import { REPOSITORIES_DIRECTORY } from 'src/utils/env';
 
 @Injectable()
 export class ProjectsService {
@@ -14,6 +16,13 @@ export class ProjectsService {
 
 	async create(project: ProjectClass): Promise<ProjectClass> {
 		const createdProject = new this.ProjectModel(project);
+
+		// Clone the Git repository
+		const git = simpleGit();
+		await git.clone(
+			createdProject.gitUrl,
+			`${REPOSITORIES_DIRECTORY}/${createdProject._id}`
+		);
 
 		return createdProject.save();
 	}
--- apps/api/src/utils/env.ts
+++ apps/api/src/utils/env.ts
@@ -9,3 +9,5 @@ export const MONGODB_URI =
 export const JWT_SECRET = process.env.JWT_SECRET || 'keyboard cat';
 export const API_HOST = process.env.API_HOST || 'http://localhost:3000';
 export const NGINX_API_URL = process.env.NGINX_API_URL || 'http://localhost:3001';
+export const REPOSITORIES_DIRECTORY =
+	process.env.REPOSITORIES_DIRECTORY || 'src/repositories';
