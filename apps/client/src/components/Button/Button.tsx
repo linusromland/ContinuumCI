8721b3fd2da471072c505ef17f2c9033043219aa
Linus Romland <hello@linusromland.com>
2023-03-11 16:03:12 +0100

    Added small option to buton

--- apps/client/src/components/Button/Button.tsx
+++ apps/client/src/components/Button/Button.tsx
@@ -1,18 +1,25 @@
+// External Dependencies
+import clsx from 'clsx';
+
 // Internal Dependencies
 import style from './Button.module.scss';
 
 export default function Button({
 	onClick,
 	disabled = false,
-	text = 'Button'
+	text = 'Button',
+	small = false,
+	className
 }: {
 	onClick: () => void;
 	disabled?: boolean;
 	text: string;
+	small?: boolean;
+	className?: string;
 }): JSX.Element {
 	return (
 		<button
-			className={style.button}
+			className={clsx(style.button, className, small ? style.small : '')}
 			onClick={() => {
 				if (!disabled) onClick();
 			}}
