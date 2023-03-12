53ebffea37f69f298857be605363dea8186f6ced
Linus Romland <hello@linusromland.com>
2023-03-12 09:21:19 +0100

    Added table to User Settings

--- apps/client/src/routes/Settings/Users/Users.tsx
+++ apps/client/src/routes/Settings/Users/Users.tsx
@@ -1,9 +1,8 @@
-// External dependencies
-import clsx from 'clsx';
-
 // Internal dependencies
 import Widget from '../../../components/Widget/Widget';
 import Breadcrumbs from '../../../components/Breadcrumbs/Breadcrumbs';
+import Table from '../../../components/Table/Table';
+import Button from '../../../components/Button/Button';
 import style from './Users.module.scss';
 
 export default function Users(): JSX.Element {
@@ -13,11 +12,33 @@ export default function Users(): JSX.Element {
 
 			<h1 className={style.title}>User Settings</h1>
 			<Widget>
-				<>
-					<div className={style.container}>
-						<h2 className={style.subtitle}>Users</h2>
-					</div>
-				</>
+				<div className={style.container}>
+					<h2 className={style.subtitle}>Users</h2>
+					<Table
+						headers={[
+							'Username',
+							'Account Type',
+							'Last Login',
+							'Last IP',
+							'Actions'
+						]}
+						data={[
+							[
+								'linusromland',
+								'Administrator',
+								'2021-05-01 12:00:00',
+								'208.19.23.23',
+								<Button
+									text='Edit'
+									small
+									secondary
+									onClick={() => console.log('Edit user')}
+								/>
+							]
+						]}
+						widget={false}
+					/>
+				</div>
 			</Widget>
 		</div>
 	);
