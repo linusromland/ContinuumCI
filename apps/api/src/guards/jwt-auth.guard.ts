08629fe8659ee9d1c4592a78e053331c7c88d40b
Linus Romland <hello@linusromland.com>
2023-03-17 17:21:00 +0100

    Saves LastIP and LastLogin in db

--- apps/api/src/guards/jwt-auth.guard.ts
+++ apps/api/src/guards/jwt-auth.guard.ts
@@ -1,14 +1,53 @@
 // External dependencies
 import {
 	ExecutionContext,
+	Inject,
 	Injectable,
 	UnauthorizedException
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import * as jwt from 'jsonwebtoken';
+import { Model } from 'mongoose';
+
+// Internal dependencies
+import { JWT_SECRET } from 'src/utils/env';
+import { UserClass } from 'shared/src/classes';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+	constructor(
+		@Inject('USER_MODEL')
+		private UserModel: Model<UserClass>
+	) {
+		super();
+	}
+
 	canActivate(context: ExecutionContext) {
+		const request = context.switchToHttp().getRequest();
+
+		const ip =
+			request.headers['x-forwarded-for'] ||
+			request.connection.remoteAddress;
+		const token = request.headers['authorization'];
+		const splitToken = token.split(' ');
+
+		if (splitToken && splitToken.length === 2) {
+			const decodedToken = jwt.verify(splitToken[1], JWT_SECRET);
+
+			if (decodedToken) {
+				(async () => {
+					const user = await this.UserModel.findById(
+						decodedToken.sub
+					);
+
+					if (user) {
+						user.lastLogin = new Date();
+						user.lastIp = ip;
+						await user.save();
+					}
+				})();
+			}
+		}
 		return super.canActivate(context);
 	}
 
