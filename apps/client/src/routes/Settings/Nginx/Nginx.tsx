87428fe3efd4249482e745bb8eebe4227fc9e1fd
Linus Romland <hello@linusromland.com>
2023-03-18 12:51:08 +0100

    Added edit modals for Nginx general settings

--- apps/client/src/routes/Settings/Nginx/Nginx.tsx
+++ apps/client/src/routes/Settings/Nginx/Nginx.tsx
@@ -1,5 +1,6 @@
 // External dependencies
 import { useEffect, useState } from 'react';
+import * as Yup from 'yup';
 import clsx from 'clsx';
 import Select from 'react-select';
 
@@ -15,10 +16,15 @@ import {
 } from '../../../utils/api/domains';
 import { toast } from 'react-toastify';
 import { DomainsClass } from 'shared/src/classes';
+import TextEditModal from '../../../components/TextEditModal/TextEditModal';
 
 export default function Nginx(): JSX.Element {
 	const [domainNames, setDomainNames] = useState([] as DomainsClass[]);
 	const [newDomainName, setNewDomainName] = useState('');
+	const [sitesEnabledDirectoryModal, setSitesEnabledDirectoryModal] =
+		useState(false);
+	const [accessLogLocationModal, setAccessLogLocationModal] = useState(false);
+	const [localIpAdressesModal, setLocalIpAdressesModal] = useState(false);
 	const [selectedDomainName, setSelectedDomainName] = useState(
 		{} as {
 			value: string;
@@ -53,101 +59,25 @@ export default function Nginx(): JSX.Element {
 	};
 
 	return (
-		<div className={style.main}>
-			<Breadcrumbs path={[{ name: 'Settings' }, { name: 'Nginx' }]} />
+		<>
+			<div className={style.main}>
+				<Breadcrumbs path={[{ name: 'Settings' }, { name: 'Nginx' }]} />
 
-			<h1 className={style.title}>Nginx Settings</h1>
-			<Widget>
-				<div className={style.container}>
-					<h2 className={style.subtitle}>Configuration</h2>
+				<h1 className={style.title}>Nginx Settings</h1>
+				<Widget>
+					<div className={style.container}>
+						<h2 className={style.subtitle}>Configuration</h2>
 
-					<div className={style.infoContainer}>
-						<h3
-							className={clsx(
-								style.infoContainerTitle,
-								style.row1,
-								style.col1
-							)}
-						>
-							Sites Enabled Directory:
-						</h3>
-						<p
-							className={clsx(
-								style.infoContainerValue,
-								style.row1,
-								style.col2
-							)}
-						>
-							/etc/nginx/sites-enabled
-						</p>
-						<h3
-							className={clsx(
-								style.infoContainerTitle,
-								style.row2,
-								style.col1
-							)}
-						>
-							Access Log Location:
-						</h3>
-						<p
-							className={clsx(
-								style.infoContainerValue,
-								style.row2,
-								style.col2
-							)}
-						>
-							/var/log/nginx/custom.log
-						</p>
-						<h3
-							className={clsx(
-								style.infoContainerTitle,
-								style.row3,
-								style.col1
-							)}
-						>
-							Local IP-Adresses:
-						</h3>
-						<p
-							className={clsx(
-								style.infoContainerValue,
-								style.row3,
-								style.col2
-							)}
-						>
-							192.168.1.0/24
-						</p>
-					</div>
-				</div>
-			</Widget>
-			<Widget>
-				<div className={style.container}>
-					<h2 className={style.subtitle}>Domain names</h2>
-
-					{/* List of all avaible domain names, button to remove a domain name & input to add a new domain name */}
-					<div className={style.infoContainer}>
-						<h3
-							className={clsx(
-								style.infoContainerTitle,
-								style.row1,
-								style.col1
-							)}
-						>
-							Available domain names:
-						</h3>
-						{/* List of all domain names */}
-						{domainNames.map((domainName, index) => (
-							<p
+						<div className={style.infoContainer}>
+							<h3
 								className={clsx(
-									style.infoContainerValue,
-									style[`row${index + 1}`],
-									style.col2
+									style.infoContainerTitle,
+									style.row1,
+									style.col1
 								)}
 							>
-								{domainName.name}
-							</p>
-						))}
-
-						{domainNames.length === 0 && (
+								Sites Enabled Directory:
+							</h3>
 							<p
 								className={clsx(
 									style.infoContainerValue,
@@ -155,102 +85,265 @@ export default function Nginx(): JSX.Element {
 									style.col2
 								)}
 							>
-								No domain names available
+								/etc/nginx/sites-enabled
 							</p>
-						)}
-					</div>
-					<div className={clsx(style.infoContainer, style.actions)}>
-						<h3
-							className={clsx(
-								style.infoContainerTitle,
-								style.row1,
-								style.col1
-							)}
-						>
-							Add domain name:
-						</h3>
-						<input
-							className={clsx(
-								style.input,
-								style.row1,
-								style.col2
-							)}
-							type='text'
-							placeholder='example.com'
-							value={newDomainName}
-							onChange={(e) => {
-								setNewDomainName(e.target.value);
-							}}
-						/>
-
-						<Button
-							text='Add'
-							small
-							secondary
-							className={clsx(style.row1, style.col3)}
-							onClick={async () => {
-								if (!newDomainName) return;
-
-								const response = await createDomain(
-									newDomainName
-								);
-
-								if (response) {
-									getDomainsData();
-									setNewDomainName('');
-								} else {
-									toast.error('Failed to create domain');
+							<Button
+								text='Change'
+								onClick={() =>
+									setSitesEnabledDirectoryModal(true)
 								}
-							}}
-						/>
+								small
+								secondary
+								className={clsx(style.row1, style.col3)}
+							/>
+							<h3
+								className={clsx(
+									style.infoContainerTitle,
+									style.row2,
+									style.col1
+								)}
+							>
+								Access Log Location:
+							</h3>
+							<p
+								className={clsx(
+									style.infoContainerValue,
+									style.row2,
+									style.col2
+								)}
+							>
+								/var/log/nginx/custom.log
+							</p>
+							<Button
+								text='Change'
+								onClick={() => setAccessLogLocationModal(true)}
+								small
+								secondary
+								className={clsx(style.row2, style.col3)}
+							/>
+							<h3
+								className={clsx(
+									style.infoContainerTitle,
+									style.row3,
+									style.col1
+								)}
+							>
+								Local IP-Adresses:
+							</h3>
+							<p
+								className={clsx(
+									style.infoContainerValue,
+									style.row3,
+									style.col2
+								)}
+							>
+								192.168.1.0/24
+							</p>
+							<Button
+								text='Change'
+								onClick={() => setLocalIpAdressesModal(true)}
+								small
+								secondary
+								className={clsx(style.row3, style.col3)}
+							/>
+						</div>
 					</div>
-					<div className={style.infoContainer}>
-						<h3
-							className={clsx(
-								style.infoContainerTitle,
-								style.row1,
-								style.col1
+				</Widget>
+				<Widget>
+					<div className={style.container}>
+						<h2 className={style.subtitle}>Domain names</h2>
+
+						{/* List of all avaible domain names, button to remove a domain name & input to add a new domain name */}
+						<div className={style.infoContainer}>
+							<h3
+								className={clsx(
+									style.infoContainerTitle,
+									style.row1,
+									style.col1
+								)}
+							>
+								Available domain names:
+							</h3>
+							{/* List of all domain names */}
+							{domainNames.map((domainName, index) => (
+								<p
+									className={clsx(
+										style.infoContainerValue,
+										style[`row${index + 1}`],
+										style.col2
+									)}
+								>
+									{domainName.name}
+								</p>
+							))}
+
+							{domainNames.length === 0 && (
+								<p
+									className={clsx(
+										style.infoContainerValue,
+										style.row1,
+										style.col2
+									)}
+								>
+									No domain names available
+								</p>
 							)}
+						</div>
+						<div
+							className={clsx(style.infoContainer, style.actions)}
 						>
-							Remove domain name:
-						</h3>
+							<h3
+								className={clsx(
+									style.infoContainerTitle,
+									style.row1,
+									style.col1
+								)}
+							>
+								Add domain name:
+							</h3>
+							<input
+								className={clsx(
+									style.input,
+									style.row1,
+									style.col2
+								)}
+								type='text'
+								placeholder='example.com'
+								value={newDomainName}
+								onChange={(e) => {
+									setNewDomainName(e.target.value);
+								}}
+							/>
 
-						<Select
-							className={clsx(style.row1, style.col2)}
-							styles={customStyles}
-							onChange={(option) => {
-								if (option && option.value)
-									setSelectedDomainName(option);
-							}}
-							value={selectedDomainName}
-							options={domainNames.map((domainName) => ({
-								value: domainName._id,
-								label: domainName.name
-							}))}
-						/>
-						<Button
-							text='Remove'
-							small
-							secondary
-							className={clsx(style.row1, style.col3)}
-							onClick={async () => {
-								if (!selectedDomainName) return;
+							<Button
+								text='Add'
+								small
+								secondary
+								className={clsx(style.row1, style.col3)}
+								onClick={async () => {
+									if (!newDomainName) return;
 
-								const response = await deleteDomain(
-									selectedDomainName.value
-								);
+									const response = await createDomain(
+										newDomainName
+									);
 
-								if (response) {
-									getDomainsData();
-									setSelectedDomainName(null);
-								} else {
-									toast.error('Failed to delete domain');
-								}
-							}}
-						/>
+									if (response) {
+										getDomainsData();
+										setNewDomainName('');
+									} else {
+										toast.error('Failed to create domain');
+									}
+								}}
+							/>
+						</div>
+						<div className={style.infoContainer}>
+							<h3
+								className={clsx(
+									style.infoContainerTitle,
+									style.row1,
+									style.col1
+								)}
+							>
+								Remove domain name:
+							</h3>
+
+							<Select
+								className={clsx(style.row1, style.col2)}
+								styles={customStyles}
+								onChange={(option) => {
+									if (option && option.value)
+										setSelectedDomainName(option);
+								}}
+								value={selectedDomainName}
+								options={domainNames.map((domainName) => ({
+									value: domainName._id,
+									label: domainName.name
+								}))}
+							/>
+							<Button
+								text='Remove'
+								small
+								secondary
+								className={clsx(style.row1, style.col3)}
+								onClick={async () => {
+									if (!selectedDomainName) return;
+
+									const response = await deleteDomain(
+										selectedDomainName.value
+									);
+
+									if (response) {
+										getDomainsData();
+										setSelectedDomainName(null);
+									} else {
+										toast.error('Failed to delete domain');
+									}
+								}}
+							/>
+						</div>
 					</div>
-				</div>
-			</Widget>
-		</div>
+				</Widget>
+			</div>
+
+			<TextEditModal
+				title='Change Sites Enabled Directory'
+				fieldName='sitesEnabledDirectory'
+				open={sitesEnabledDirectoryModal}
+				onClose={() => {
+					setSitesEnabledDirectoryModal(false);
+				}}
+				initialValues={{
+					sitesEnabledDirectory: 'sitesEnabledDirectory'
+				}}
+				validationSchema={Yup.object().shape({
+					sitesEnabledDirectory: Yup.string().required(
+						'Sites Enabled Directory is required'
+					)
+				})}
+				submit={async (values) => {
+					console.log(values);
+				}}
+			/>
+
+			<TextEditModal
+				title='Change Access Log Location'
+				fieldName='accessLogLocation'
+				open={accessLogLocationModal}
+				onClose={() => {
+					setAccessLogLocationModal(false);
+				}}
+				initialValues={{
+					accessLogLocation: 'accessLogLocation'
+				}}
+				validationSchema={Yup.object().shape({
+					accessLogLocation: Yup.string().required(
+						'Access Log Location is required'
+					)
+				})}
+				submit={async (values) => {
+					console.log(values);
+				}}
+			/>
+
+			<TextEditModal
+				title='Change Local IP-Adresses'
+				fieldName='localIpAdresses'
+				open={localIpAdressesModal}
+				onClose={() => {
+					setLocalIpAdressesModal(false);
+				}}
+				initialValues={{
+					localIpAdresses: 'localIpAdresses'
+				}}
+				validationSchema={Yup.object().shape({
+					localIpAdresses: Yup.string().required(
+						'Local IP-Adresses is required'
+					)
+				})}
+				submit={async (values) => {
+					console.log(values);
+				}}
+			/>
+		</>
 	);
 }
