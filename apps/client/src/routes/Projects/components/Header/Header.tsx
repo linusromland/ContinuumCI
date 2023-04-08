82e7e0ccb8d4337d37f2cea6e69f11eab045effe
Linus Romland <hello@linusromland.com>
2023-04-08 16:01:55 +0200

    Fixed animated spinner in header on projects

--- apps/client/src/routes/Projects/components/Header/Header.tsx
+++ apps/client/src/routes/Projects/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 // External dependencies
 import dayjs from 'dayjs';
+import { useState } from 'react';
 
 // Internal dependencies
 import style from './Header.module.scss';
 
 export default function Header({ lastUpdated, onRefresh }: { lastUpdated: string; onRefresh: () => void }) {
+	const [refreshing, setRefreshing] = useState(false as boolean);
+
 	return (
 		<header className={style.header}>
 			<h1>Projects</h1>
@@ -12,8 +15,15 @@ export default function Header({ lastUpdated, onRefresh }: { lastUpdated: string
 				<p>
 					Last updated: <span>{lastUpdated ? dayjs(lastUpdated).format('HH:mm:ss') : 'Never'}</span>
 				</p>
-				<button onClick={onRefresh}>
+				<button
+					onClick={async () => {
+						setRefreshing(true);
+						await onRefresh();
+						setRefreshing(false);
+					}}
+				>
 					<img
+						className={refreshing ? style.refreshing : ''}
 						src='/icons/refresh.svg'
 						alt='Refresh'
 					/>
