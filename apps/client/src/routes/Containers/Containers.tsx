d24d4b1e23f6b644db1d529e8e1763559f07d274
Linus Romland <hello@linusromland.com>
2023-04-15 15:21:43 +0200

    Added loading state to Containers

--- apps/client/src/routes/Containers/Containers.tsx
+++ apps/client/src/routes/Containers/Containers.tsx
@@ -9,48 +9,67 @@ import Widget from '../../components/Widget/Widget';
 import Table from '../../components/Table/Table';
 import { getContainers } from '../../utils/api/containers';
 import { ContainerType } from 'shared/src/types';
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
+import { Loading } from '../../components/Loading/Loading';
 
 export default function Containers(): JSX.Element {
 	const [containers, setContainers] = useState([] as ContainerType[]);
+	const [dataReady, setDataReady] = useState(false);
 
 	useEffect(() => {
 		getData();
 	}, []);
 
 	async function getData() {
+		setDataReady(false);
 		const response = await getContainers([]);
 		if (response.success && response.data) {
 			setContainers(response.data);
 		}
+		setDataReady(true);
 	}
 
+	if (!dataReady) return <Loading />;
+
 	return (
-		<div className={style.main}>
-			{containers && containers.length > 0 && (
-				<>
-					<h1 className={style.title}>Containers</h1>
-					<Widget contentClass={style.contentClass}>
-						<div className={style.container}>
-							<h2 className={style.subtitle}>Available containers</h2>
-							<Table
-								widget={false}
-								headers={['Name', 'State', 'Created']}
-								data={containers.map((container) => [
-									<Link to={`/containers/${container.id}`}>{container.name}</Link>,
-									container.state,
-									container.created ? dayjs(container.created * 1000).format('YYYY-MM-DD HH:mm') : '-'
-								])}
-							/>
-						</div>
-					</Widget>
-				</>
-			)}
-			{containers && containers.length === 0 && (
-				<div className={style.container}>
-					<h2 className={style.subtitle}>Containers</h2>
-					<p>No containers found.</p>
-				</div>
-			)}
-		</div>
+		<main className={style.main}>
+			<Breadcrumbs
+				path={[
+					{
+						name: 'Containers',
+						link: '/conatiners'
+					}
+				]}
+			/>
+			<div className={style.content}>
+				{containers && containers.length > 0 && (
+					<>
+						<h1 className={style.title}>Containers</h1>
+						<Widget contentClass={style.contentClass}>
+							<div className={style.container}>
+								<h2 className={style.subtitle}>Available containers</h2>
+								<Table
+									widget={false}
+									headers={['Name', 'State', 'Created']}
+									data={containers.map((container) => [
+										<Link to={`/containers/${container.id}`}>{container.name}</Link>,
+										container.state,
+										container.created
+											? dayjs(container.created * 1000).format('YYYY-MM-DD HH:mm')
+											: '-'
+									])}
+								/>
+							</div>
+						</Widget>
+					</>
+				)}
+				{containers && containers.length === 0 && (
+					<>
+						<h2 className={style.title}>Containers</h2>
+						<p>No containers found.</p>
+					</>
+				)}
+			</div>
+		</main>
 	);
 }
