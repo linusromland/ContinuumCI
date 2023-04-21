7773a8d15e7a72a913fd1591f6b4797b063c24cc
Linus Romland <hello@linusromland.com>
2023-04-21 14:13:03 +0200

    Added action buttons to unverify banner

--- apps/client/src/components/Layouts/MainLayout/MainLayout.tsx
+++ apps/client/src/components/Layouts/MainLayout/MainLayout.tsx
@@ -8,24 +8,40 @@ import Sidebar from '../../Sidebar/Sidebar';
 import { getUser } from '../../../utils/api/user';
 import { UserClass } from 'shared/src/classes';
 import UnverifiedBanner from '../../UnverifiedBanner/UnverifiedBanner';
+import { toast } from 'react-toastify';
 
 export default function MainLayout(): JSX.Element {
 	const [user, setUser] = useState({} as UserClass);
 
 	useEffect(() => {
-		(async () => {
-			const userResponse = await getUser();
-			const user = userResponse.data as UserClass;
-
-			if (user) {
-				setUser(user);
-			}
-		})();
+		getUserData();
 	}, []);
+
+	async function getUserData() {
+		const userResponse = await getUser();
+		const user = userResponse.data as UserClass;
+
+		if (user) {
+			setUser(user);
+
+			return user;
+		}
+
+		return null;
+	}
 
 	return (
 		<div className={style.mainLayoutWrapper}>
-			{user.verifiedEmail === false && <UnverifiedBanner />}
+			{user.verifiedEmail === false && (
+				<UnverifiedBanner
+					recheckStatus={async () => {
+						const user = await getUserData();
+						if (user?.verifiedEmail) {
+							toast.success('Your email has been verified!');
+						}
+					}}
+				/>
+			)}
 			<div className={style.mainLayout}>
 				<Sidebar user={user} />
 				<Outlet />
--- apps/client/src/components/UnverifiedBanner/UnverifiedBanner.tsx
+++ apps/client/src/components/UnverifiedBanner/UnverifiedBanner.tsx
@@ -1,7 +1,12 @@
 // Internal Dependencies
+import Button from '../Button/Button';
 import style from './UnverifiedBanner.module.scss';
 
-export default function UnverifiedBanner() {
+type UnverifiedBannerProps = {
+	recheckStatus: () => void;
+};
+
+export default function UnverifiedBanner({ recheckStatus }: UnverifiedBannerProps) {
 	return (
 		<div className={style.unverifiedBanner}>
 			<img
@@ -9,6 +14,22 @@ export default function UnverifiedBanner() {
 				alt='Warning'
 			/>
 			<p>Your email address has not been verified. Please check your email for a verification link.</p>
+			<div className={style.actions}>
+				<Button
+					text='Resend email'
+					theme='secondary'
+					onClick={() => {
+						console.log('Resend email');
+					}}
+					small
+				/>
+				<Button
+					text='Check verification status'
+					theme='secondary'
+					onClick={recheckStatus}
+					small
+				/>
+			</div>
 		</div>
 	);
 }
