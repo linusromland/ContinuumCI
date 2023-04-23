c939d9f3f6737937fca452a8265c95ec727fff16
Linus Romland <hello@linusromland.com>
2023-04-23 18:11:07 +0200

    Created swedish translations

--- apps/client/src/i18n/locales/sv.locale.ts
+++ apps/client/src/i18n/locales/sv.locale.ts
@@ -2,5 +2,53 @@
 import { TranslationKeys } from './locale.type';
 
 export const sv: TranslationKeys = {
-	hello_world: 'Hej, världen!'
+	setupSidebar: {
+		footer: 'ContinuumCI är ett projekt av Linus Romland. Källkoden är tillgänglig på'
+	},
+	resetPassword: {
+		title: 'Återställ lösenord',
+		successReset:
+			'Om e-postadressen är giltig kommer du att få ett e-postmeddelande med en länk för att återställa ditt lösenord.',
+		errorReset: 'Ett fel uppstod vid återställning av ditt lösenord. Försök igen senare.'
+	},
+	login: {
+		title: 'Välkommen tillbaka! Vänligen logga in på ditt konto.',
+		successLogin: 'Du har loggat in!',
+		invalidLogin: 'Ogiltig e-postadress eller lösenord.',
+		rememberMe: 'Kom ihåg mig',
+		forgotPassword: 'Glömt lösenordet?',
+		resetPassword: 'Återställ lösenord',
+		notRegistered: 'Har du inget konto?',
+		login: 'Logga in',
+		password: 'Lösenord'
+	},
+	register: {
+		title: 'Välkommen! Vänligen registrera ett konto.',
+		successCreate: 'Ditt konto har skapats!',
+		errorCreate: 'Ett fel uppstod vid skapande av ditt konto. Försök igen senare.',
+		alreadyRegistered: 'Har du redan ett konto?',
+		username: 'Användarnamn',
+		confirmPassword: 'Bekräfta lösenordet',
+		create: 'Skapa konto',
+		register: 'Registrera',
+		schema: {
+			username: {
+				min: 'Användarnamnet måste vara minst 3 tecken långt.',
+				max: 'Användarnamnet får vara högst 20 tecken långt.',
+				required: 'Användarnamn krävs.'
+			},
+			email: {
+				required: 'E-postadress krävs.',
+				invalid: 'E-postadressen är ogiltig.'
+			},
+			password: {
+				min: 'Lösenordet måste vara minst 8 tecken långt.',
+				required: 'Lösenord krävs.'
+			},
+			confirmPassword: {
+				notMatch: 'Lösenorden matchar inte.',
+				required: 'Bekräfta lösenord krävs.'
+			}
+		}
+	}
 };
