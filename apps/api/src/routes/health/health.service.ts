50e695d65154e2a20403fa3b02053d0014dd8b9b
Linus Romland <hello@linusromland.com>
2023-04-29 14:44:00 +0200

    Added nginx check for the health endpoint in normal api

--- apps/api/src/routes/health/health.service.ts
+++ apps/api/src/routes/health/health.service.ts
@@ -1,12 +1,31 @@
 // External dependencies
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 // Internal dependencies
 import { ResponseType } from 'shared/src/types';
+import { NGINX_API_URL } from 'src/utils/env';
 
 @Injectable()
 export class HealthService {
-	healthCheck(): ResponseType {
+	async healthCheck(): Promise<ResponseType> {
+		const request = await axios.get(
+			`${NGINX_API_URL}/health`,
+
+			{
+				validateStatus: () => true
+			}
+		);
+
+		const data = request.data as ResponseType;
+
+		if (!data || !data.success) {
+			throw new BadRequestException({
+				success: false,
+				message: 'Api is running but nginx api is not'
+			});
+		}
+
 		return {
 			success: true,
 			message: 'API is running'
